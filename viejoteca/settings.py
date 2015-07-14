ff6a5401a4d5bcd79a912deca7888212338baa71
YOHAN <jsarias0514@gmail.com>
2015-07-14 13:40:49 -0500

    Removed math filters

--- viejoteca/settings.py
+++ viejoteca/settings.py
@@ -46,7 +46,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = (
         'storages',
-        'mathfilters',
+        # 'mathfilters',
         'django.contrib.admin',
         'django.contrib.auth',
         'django.contrib.contenttypes',
